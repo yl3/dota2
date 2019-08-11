b099a780b1aa1b133c1f14150a6600f9661c8d08
Yilong Li <yilong.li.yl3@gmail.com>
2019-08-11 15:06:37 -0400

    Added code for plotting ROC curves

--- src/stats.py
+++ src/stats.py
@@ -7,6 +7,7 @@ import plotly.graph_objects as plotly_go
 from poibin import PoiBin
 import scipy.stats
 import scipy.spatial.distance
+import sklearn.metrics
 
 from .models import gp
 from . import load
@@ -74,23 +75,55 @@ def win_oe_pval_series(win_probs, outcomes):
     return pd.Series(res, index=idx)
 
 
+def _prediction_to_plotly_roc_data(y_true, y_pred, name=None):
+    """Return a Plotly data object for plotting a ROC curve."""
+    fpr, tpr, thres = sklearn.metrics.roc_curve(y_true, y_pred)
+    auc = sklearn.metrics.roc_auc_score(y_true, y_pred)
+    if name is None:
+        name = "AUC: {:.3f}".format(auc)
+    else:
+        name = name + " ({:.3f})".format(auc)
+    data = plotly_go.Scatter(x=fpr, y=tpr, mode='lines', name=name,
+                             showlegend=True, hovertext=np.round(thres, 3))
+    return data
+
+
+def _roc_curve_plotly_plot(data):
+    """Create a Plotly ROC curve plot.
+
+    Args:
+        data (list): List of `plotly.graph_objs.Scatter` objects for
+            plotly.Figure.
+    """
+    layout = plotly_go.Layout(
+        title='ROC curves',
+        xaxis=dict(title='Cumulative proportion of all Dire wins'),
+        yaxis=dict(title='Cumulative proportion of all Radiant wins'))
+    # Add a 0, 1 line.
+    null_trace = plotly_go.Scatter(x=[0, 1], y=[0, 1], mode='lines',
+                                   line=dict(color='grey', dash='dash'),
+                                   showlegend=False)
+    return plotly_go.Figure(data=data + [null_trace], layout=layout)
+
+
 class MatchPred:
     """Class for storing and analysing match predictions."""
 
-    def __init__(self, matches, match_pred, skills_mat=None):
+    def __init__(self, matches, match_pred, logistic_scale, skills_mat=None):
+        self.logistic_scale = logistic_scale
+
         self._validate_match_pred(match_pred)
-        self.match_pred = match_pred
-        if skills_mat is not None:
-            self.skills_mat = skills_mat.reindex(self.match_pred.index)
-        else:
-            skills_mat = None
+        self.match_pred = self._add_unknown_side_win_prob(match_pred)
         assert isinstance(matches, load.MatchDF)
         self.matches = load.MatchDF(matches.df.reindex(self.match_pred.index))
 
         # Need to re-reindex everything, since load.MatchDF sorts the data
         # frame by match ID..
         self.match_pred = self.match_pred.loc[self.matches.df.index]
-        self.skills_mat = self.skills_mat.loc[self.matches.df.index]
+        if skills_mat is not None:
+            self.skills_mat = skills_mat.loc[self.matches.df.index]
+        else:
+            self.skills_mat = None
 
         self._hovertext = None
 
@@ -99,6 +132,9 @@ class MatchPred:
         if self._hovertext is None:
             self._hovertext = self._matches_to_hovertext()
         return self._hovertext
+
+    def win_prob(self, skill_diffs):
+        return gp.logistic_win_prob(skill_diffs, self.logistic_scale)
 
     def player_skills_plot(self, player_ids):
         """Create a Plotly figure showing players' skills."""
@@ -149,16 +185,57 @@ class MatchPred:
         fig = plotly_go.Figure(data=plotly_data, layout=layout)
         return fig
 
-    def match_pred_df_bias(self, win_prob_breaks=np.arange(0.0, 1.1, 0.1),
-                           iloc=slice(None), loc=None):
-        """Compute prediction bias in a match prediction data frame."""
-        temp = self.match_pred[["pred_win_prob"]].merge(
-            self.matches.df[["radiantVictory"]], left_index=True,
-            right_index=True)
-        if loc is None:
-            temp = temp.iloc[iloc]
+    def roc_curve_plot(self, side_known=True, loc_dict=None, iloc_dict=None):
+        """Plot a ROC curve of the current predictions.
+
+        Args:
+            side_known (bool): Assume that the side of each team is known?
+            loc_dict (dict): A dictionary of slices for slicing matches.
+            iloc_dict (dict): A dictionary of iloc slices for slicing matches.
+        """
+        traces = []
+        y_true = self.matches.df.radiantVictory
+        if side_known:
+            y_pred = self.match_pred.pred_win_prob
         else:
-            temp = temp.loc[loc]
+            y_pred = self.match_pred.pred_win_prob_unknown_side
+        print(y_pred.head(10))
+        if loc_dict is not None:
+            for name, loc in loc_dict.items():
+                trace = _prediction_to_plotly_roc_data(
+                    y_true[loc], y_pred[loc], name)
+                traces.append(trace)
+        elif iloc_dict is not None:
+            for name, iloc in iloc_dict.items():
+                trace = _prediction_to_plotly_roc_data(
+                    y_true.iloc[iloc], y_pred.iloc[iloc], name)
+                traces.append(trace)
+        else:
+            trace = _prediction_to_plotly_roc_data(y_true, y_pred)
+            traces.append(trace)
+        return _roc_curve_plotly_plot(traces)
+
+    def match_pred_df_bias(self, win_prob_breaks=np.arange(0.0, 1.1, 0.1),
+                           iloc=slice(None), loc=None, side_known=True):
+        """Compute prediction bias in a match prediction data frame."""
+        if side_known:
+            temp = self.match_pred[["pred_win_prob"]].merge(
+                self.matches.df[["radiantVictory"]], left_index=True,
+                right_index=True)
+            if loc is None:
+                temp = temp.iloc[iloc]
+            else:
+                temp = temp.loc[loc]
+        else:
+            temp = self.match_pred[["pred_win_prob_unknown_side"]].merge(
+                self.matches.df[["radiantVictory"]], left_index=True,
+                right_index=True)
+            temp.rename(columns={'pred_win_prob_unknown_side': 'pred_win_prob'},
+                        inplace=True)
+            if loc is None:
+                temp = temp.iloc[iloc]
+            else:
+                temp = temp.loc[loc]
         win_prob_interval = \
             pd.cut(temp.pred_win_prob, win_prob_breaks, include_lowest=True)
         grouped = temp.groupby(win_prob_interval)
@@ -167,7 +244,7 @@ class MatchPred:
                 win_oe_pval_series(df.pred_win_prob, df.radiantVictory),
                 name=name)
              for name, df in grouped])
-        res_df.loc[:, "exp_win"] = res_df.loc[:, "exp_win"].round(2)
+        res_df.loc[:, "exp_win"] = res_df.loc[:, "exp_win"].round(3)
         return res_df
 
     def team_skills(self):
@@ -202,6 +279,17 @@ class MatchPred:
             'radi_adv'])
         if not all(match_pred.columns.sort_values() == expected_columns):
             raise ValueError("Unexpected columns in match_pred.")
+
+    def _add_unknown_side_win_prob(self, match_pred):
+        """
+        Add a column called 'pred_win_prob_unknown_side' for win probability
+        given unknown side each team plays on.
+        """
+        skill_diff = match_pred.radi_skill - match_pred.dire_skill
+        radi_adv = match_pred.radi_adv
+        y_pred = (self.win_prob(skill_diff + radi_adv) / 2
+                  + self.win_prob(skill_diff - radi_adv) / 2)
+        return match_pred.assign(pred_win_prob_unknown_side=y_pred)
 
     def _match_entry_to_hovertext(self, match, prediction, cur_skills):
         """Convert one entry of a match with predictions to hovertext.
