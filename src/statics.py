53821e8ca8e74554785ba96d9d22c1bec479d52f
Yilong Li <yilong.li.yl3@gmail.com>
2019-09-06 00:39:15 -0400

    Add report on TI9 group stage results

--- src/statics.py
+++ src/statics.py
@@ -1,23 +1,23 @@
 """Static variables."""
 
 
-TI9_TEAM_NAMES = [
-    "Alliance",
-    "Chaos Esports Club",
-    "Evil Geniuses",
-    "Fnatic",
-    "Forward Gaming",
-    "Infamous Gaming",
-    "Mineski",
-    "Natus Vincere",
-    "Ninjas in Pyjamas",
-    "Newbee",
-    "OG",
-    "PSG.LGD",
-    "Royal Never Give Up",
-    "Team Liquid",
-    "Team Secret",
-    "TNC Predator",
-    "Vici Gaming",
-    "Virtus.pro",
-]
+TI9_TEAM_NAMES = {
+    "Alliance": 111474,
+    "Chaos Esports Club": 7203342,
+    "Evil Geniuses": 39,
+    "Fnatic": 350190,
+    "Newbee": 6214538,
+    "Infamous Gaming": 2672298,
+    "KEEN GAMING": 2626685,
+    "Mineski": 543897,
+    "Natus Vincere": 36,
+    "Ninjas in Pyjamas": 6214973,
+    "OG": 2586976,
+    "PSG.LGD": 15,
+    "Royal Never Give Up": 6209804,
+    "Team Liquid": 2163,
+    "Team Secret": 1838315,
+    "TNC Predator": 2108395,
+    "Vici Gaming": 726228,
+    "Virtus.pro": 1883502,
+}
